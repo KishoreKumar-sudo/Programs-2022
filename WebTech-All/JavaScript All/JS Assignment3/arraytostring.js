be2473ce496fafcbcc3ff2b61062fc24b758cf37
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2022-12-16 13:17:30 +0530

    hello

---
+++ WebTech-All/JavaScript All/JS Assignment3/arraytostring.js
@@ -0,0 +1,4 @@
+const fruits=["Apple", "Banana", "Orange", "Mango", "Kiwi"];
+let text = fruits.toString()
+console.log(fruits)
+//console.log(text)
---
+++ WebTech-All/JavaScript All/JS Assignment3/vowel.js
@@ -0,0 +1,14 @@
+let s = "KishoreKumar"
+let c, i
+for (let i = 0; i <= s.length - 1; i++) {
+   c = s[i]
+
+   if (c == "a" || c == "e" || c == "i" || c == "o" || c == "u" ||
+      c == "A" || c == "E" || c == "I" || c == "O" || c == "U") {
+      console.log(c)
+   }
+}
+
+
+    //  let a=10;
+    //  console.log(a)
